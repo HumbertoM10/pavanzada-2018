8cf3c11fd025b802011bb6ead104acf6fc79ccf6
HumbertoM10 <humberto_e10@hotmail.es>
2018-04-20 07:15:52 -0500

    Se agregaron los codigos referentes a threads (Practica 14)

---
+++ Practicas/Practica14/pthread2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define NUM_THREADS 20
+void *helloWorld(void *p){
+	long id = (long)p;
+	char *msg = (char *)malloc(25*sizeof(char));
+	sprintf(msg, "Hello World %ld\n",id);
+	return (void *)msg;
+}
+
+int main(){
+	pthread_t threads[NUM_THREADS];
+	long i;
+	char *msgResult;
+
+	for(i = 0; i<NUM_THREADS; i++){
+		pthread_create(
+			&threads[i],
+			NULL,
+			helloWorld,
+			(void *)i);
+	}
+	for(i = 0; i<NUM_THREADS; i++){
+		pthread_join(
+			threads[i],
+			(void *)&msgResult);
+		printf("%s", msgResult);
+		free(msgResult);
+	}
+	return 0;
+}
